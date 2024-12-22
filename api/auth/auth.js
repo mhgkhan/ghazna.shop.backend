266809842883cb32822e9675c83d55f04318e0a5
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2024-12-22 16:50:40 +0500

    added auth api file

---
+++ api/auth/auth.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const authRoute = express.Router();
+
+authRoute.get("/user", (req,res)=>res.status(200).json({success:true,message:"user is here"}))
+
+export default authRoute
+
--- api/index.js
+++ api/index.js
@@ -1,8 +1,29 @@
-const express = require("express");
+import express from "express";
+import os from "os"
+import cluster from "cluster"
+import authRoute from "./auth/auth.js";
+
 const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
-app.get("/", (req, res) => res.send("hello gh"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.use((req,res,next)=>{
+    req.user = req.headers.location;
+    next();
+})
 
-module.exports = app;
+app.use("/api/auth/", authRoute)
+
+
+app.get("/", (req, res) => {
+    res.status(200).json({message: "hello world" })
+});
+
+if (cluster.isPrimary) {
+    for (let i = 0; i < os.cpus().length; i++) cluster.fork()
+}
+else {
+    app.listen(3090, () => console.log("Server ready on port 3090."));
+}
+export default app;
