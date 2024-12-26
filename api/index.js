0c00a143f0ac44faf40c79255f06cade03f5e5dd
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2024-12-26 20:25:39 +0500

    CORS REMOVED

--- api/index.js
+++ api/index.js
@@ -9,34 +9,16 @@ import cors from "cors";
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://ghazna.shop'); // Replace with your frontend URL
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
-
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(cors({ origin: 'https://ghazna.shop/' }))
 
-
-app.use((req, res, next) => {
-    req.user = req.headers.location;
-    next();
-})
 
 app.use("/api/auth/", authRoute)
 
 app.get("/", (req, res) => {
     res.status(200).json({ website: "https://ghazna.shop" })
 });
-
-
-
-
 
 
 
